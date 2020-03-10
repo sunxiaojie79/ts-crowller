4ddf1ebdef8d6752c6822093408c83bd9cf76473
sunxiaojie79 <297280911@qq.com>
2020-03-10 18:05:51 +0800

    feat:爬取数据的结构设计及存储

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import sueragent from 'superagent';
 import cheerio from 'cheerio';
 
@@ -5,11 +7,35 @@ interface Book {
   title: string,
   price: number
 }
+
+interface BookResults {
+  time: number;
+  data: Book[];
+}
+
+interface Content {
+  [propName: number]: Book[]
+}
 class Crowller {
   private url = 'https://book.douban.com/';
+
   constructor () {
-    this.getRowHtml(this.url);
+    this.initSpiderProcess();
   }
+
+  async initSpiderProcess () {
+    let filePath = path.resolve(__dirname, '../data/book.json');
+    const html = await this.getRowHtml(this.url);
+    const info = this.getBookInfo(html);
+    const fileContent = this.generateJsonContent(info);
+    fs.writeFileSync(filePath, JSON.stringify(fileContent));
+  }
+  
+  async getRowHtml (url: string) {
+    const result = await sueragent.get(url);
+    return result.text;
+  }
+
   getBookInfo (html: string) {
     const $ = cheerio.load(html);
     const bookItems = $('.market-books .list-col .info');
@@ -17,17 +43,22 @@ class Crowller {
     bookItems.map((index, element) => {
       const title = $(element).find('.title a').text();
       const price = parseInt($(element).find('.price').text().split('￥')[1], 10);
-      bookInfos.push({title, price})
+      bookInfos.push({title, price});
     });
-    const result = {
+    return {
       time: new Date().getTime(),
       data: bookInfos
     }
-    console.log(result)
   }
-  async getRowHtml (url: string) {
-    const result = await sueragent.get(url);
-    this.getBookInfo(result.text);
+
+  generateJsonContent (info: BookResults) {
+    let filePath = path.resolve(__dirname, '../data/book.json');
+    let fileContent: Content = {}
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    }
+    fileContent[info.time] = info.data;
+    return fileContent;
   }
 }
 
