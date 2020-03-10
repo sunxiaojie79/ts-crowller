ce64dcc8996cce3442dfdba8dc991fe79355eedf
sunxiaojie79 <297280911@qq.com>
2020-03-10 17:29:12 +0800

    feat:使用cheerio进行数据爬取

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,13 +1,33 @@
 import sueragent from 'superagent';
+import cheerio from 'cheerio';
+
+interface Book {
+  title: string,
+  price: number
+}
 class Crowller {
-  private url = 'https://movie.douban.com/';
-  private rowHtml = ''
+  private url = 'https://book.douban.com/';
   constructor () {
     this.getRowHtml(this.url);
   }
+  getBookInfo (html: string) {
+    const $ = cheerio.load(html);
+    const bookItems = $('.market-books .list-col .info');
+    const bookInfos: Book[]= [];
+    bookItems.map((index, element) => {
+      const title = $(element).find('.title a').text();
+      const price = parseInt($(element).find('.price').text().split('￥')[1], 10);
+      bookInfos.push({title, price})
+    });
+    const result = {
+      time: new Date().getTime(),
+      data: bookInfos
+    }
+    console.log(result)
+  }
   async getRowHtml (url: string) {
     const result = await sueragent.get(url);
-    this.rowHtml = result.text;
+    this.getBookInfo(result.text);
   }
 }
 
