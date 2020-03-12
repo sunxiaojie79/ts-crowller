78f9b1612bb14a447128a919961148c1379b1b11
sunxiaojie79 <297280911@qq.com>
2020-03-12 16:00:49 +0800

    feat:引入bodyparser

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response} from 'express';
 import router from './router';
+import bodyParser from 'body-parser';
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(router);
 
--- src/router.ts
+++ src/router.ts
@@ -4,11 +4,27 @@ import BookAnalyzer from './bookAnalyzer'
 
 const router = Router();
 
-router.get('/getData', (req: Request, res: Response) => {
-  const url = 'https://book.douban.com/';
-  const analyzer = BookAnalyzer.getInstance();
-  new Crowller(url, analyzer);
-  res.send('get data');
+router.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <html>
+      <body>
+        <form method="post" action="/getData">
+          <input type="password" name="password"/>
+          <button>提交</button>
+        </form>
+      </body>
+    </html>
+  `)
+});
+router.post('/getData', (req: Request, res: Response) => {
+  if (req.body.password === '123') {
+    const url = 'https://book.douban.com/';
+    const analyzer = BookAnalyzer.getInstance();
+    new Crowller(url, analyzer);
+    res.send('get data success');
+  } else {
+    res.send('get data fail');
+  }
 });
 
 export default router;
