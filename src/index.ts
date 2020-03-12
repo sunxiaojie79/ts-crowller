61feb234356c2686b5a3c27762685de93c0b3c7c
sunxiaojie79 <297280911@qq.com>
2020-03-12 18:05:53 +0800

    feat:登录功能

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response, NextFunction} from 'express';
 import router from './router';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  maxAge: 24 * 60 * 60 * 1000
+}))
 app.use((req: Request, res: Response, next: NextFunction) => {
   req.customProp = 'this is a custom property';
   next();
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response} from 'express';
 import Crowller from './crowller';
-import BookAnalyzer from './bookAnalyzer'
+import BookAnalyzer from './bookAnalyzer';
+import fs from 'fs';
+import path from 'path';
 
 const router = Router();
 
@@ -11,20 +13,56 @@ interface RequestWithBody extends Request {
 }
 
 router.get('/', (req: RequestWithBody, res: Response) => {
-  res.send(`
-    <html>
-      <body>
-        <form method="post" action="/getData">
-          <input type="password" name="password"/>
-          <button>提交</button>
-        </form>
-      </body>
-    </html>
-  `)
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    res.send(`
+      <html>
+        <body>
+          <a href="/getData"> 爬取数据</a>
+          <a href="/showData"> 展示数据</a>
+          <a href="/logout"> 退出</a>
+        </body>
+      </html>
+    `)
+  } else {
+    res.send(`
+      <html>
+        <body>
+          <form method="post" action="/login">
+            <input type="password" name="password"/>
+            <button>提交</button>
+          </form>
+        </body>
+      </html>
+    `)
+  }
 });
-router.post('/getData', (req: RequestWithBody, res: Response) => {
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const isLogin = req.session ? req.session.login : false;
   const {password} = req.body
-  if (password === '123') {
+  if (isLogin) {
+    res.redirect('/');
+  } else {
+    if (password === '123' && req.session) {
+      req.session.login = true
+      res.send('login success');
+    } else {
+      res.send(`${req.customProp}, login fail`);
+    }
+  }
+});
+
+router.get('/logout', (req: RequestWithBody, res: Response) => {
+  if (req.session) {
+    req.session.login = undefined
+  }
+  res.redirect('/')
+});
+
+router.get('/getData', (req: RequestWithBody, res: Response) => {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
     const url = 'https://book.douban.com/';
     const analyzer = BookAnalyzer.getInstance();
     new Crowller(url, analyzer);
@@ -34,4 +72,18 @@ router.post('/getData', (req: RequestWithBody, res: Response) => {
   }
 });
 
+router.get('/showData', (req: RequestWithBody, res: Response) => {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    try {
+      const filePath = path.resolve(__dirname, '../data/book.json');
+      const content = fs.readFileSync(filePath, 'utf-8');
+      res.send(JSON.parse(content));
+    } catch (e){
+      res.send(`${e}, 尚未爬取到内容`);
+    }
+  } else {
+    res.send('请登录后查看');
+  }
+});
 export default router;
