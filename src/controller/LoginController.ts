83a6529e9d9951468ee3bd7e10f13cc8d1060ea3
sunxiaojie79 <297280911@qq.com>
2020-03-17 18:14:59 +0800

    feat:登陆退出功能完整优化

--- src/controller/LoginController.ts
+++ src/controller/LoginController.ts
@@ -9,13 +9,13 @@ interface RequestWithBody extends Request {
   };
 }
 
-@controller('/')
+@controller('/api')
 export class LoginController {
   static isLogin (req: RequestWithBody): boolean {
     return !!(req.session ? req.session.login : false);
   }
 
-  @get('/api/isLogin')
+  @get('/isLogin')
   isLogin(req: RequestWithBody, res: Response): void{
     const isLogin = LoginController.isLogin(req);
     res.json(getResponseData(isLogin));
@@ -34,7 +34,7 @@ export class LoginController {
     const isLogin = LoginController.isLogin(req);
     const {password} = req.body
     if (isLogin) {
-      res.send(getResponseData(false, `${req.customProp}, 已登录`));
+      res.send(getResponseData(true, `${req.customProp}, 已登录`));
     } else {
       if (password === '123' && req.session) {
         req.session.login = true
@@ -42,33 +42,6 @@ export class LoginController {
       } else {
         res.send(getResponseData(false, `${req.customProp}, 登录失败`));
       }
-    }
-  }
-
-  @get('/')
-  home(req: RequestWithBody, res: Response): void{
-    const isLogin = LoginController.isLogin(req);
-    if (isLogin) {
-      res.send(`
-        <html>
-          <body>
-            <a href="/getData"> 爬取数据</a>
-            <a href="/showData"> 展示数据</a>
-            <a href="/logout"> 退出</a>
-          </body>
-        </html>
-      `)
-    } else {
-      res.send(`
-        <html>
-          <body>
-            <form method="post" action="/login">
-              <input type="password" name="password"/>
-              <button>提交</button>
-            </form>
-          </body>
-        </html>
-      `)
     }
   }
 }
